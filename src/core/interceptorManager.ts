24257aa27b5f7f59e2c3f1122a49fb649b6d6bd2
qinweijian <qinweijian@lrhealth.com>
2021-04-12 18:15:39 +0800

    20210412

--- src/core/interceptorManager.ts
+++ src/core/interceptorManager.ts
@@ -1,3 +1,36 @@
-// export default class InterceptorManager {
-//     private interceptors
-// }
+import { ResolveFn, RejectedFn } from '../types'
+
+interface Interceptor<T> {
+    resolved: ResolveFn<T>
+    rejected?: RejectedFn
+}
+
+export default class InterceptorManager<T> {
+    private interceptors:Array<Interceptor<T> | null>
+
+    constructor(){
+        this.interceptors = []
+    }
+
+    use(resolved:ResolveFn<T>, rejected?:RejectedFn): number {
+        this.interceptors.push({
+            resolved,
+            rejected
+        })
+        return this.interceptors.length - 1
+    }
+
+    forEach(fn: (interceptor: Interceptor<T>) => void) : void {
+        this.interceptors.forEach(interceptor=>{
+            if (interceptor !== null) {
+                fn(interceptor)
+            }
+        })
+    }
+
+    eject(id: number): void {
+        if (this.interceptors[id]) {
+            this.interceptors[id] = null
+        }
+    }
+}
